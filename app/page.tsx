0c677a57d98c5cda7fecf21496a85a1b5a7fba2d
Ward D Support <121932307+Jakquellyn@users.noreply.github.com>
2023-10-01 15:04:01 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 			<main className="flex min-h-screen flex-col items-center justify-between p-24">
 				<div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
 					<div className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-						<h1 className="text-4xl font-bold">Welocome dear luv</h1>
+						<h1 className="text-4xl font-bold">Welocome To Calestial</h1>
 					</div>
 				</div>
 			</main>
